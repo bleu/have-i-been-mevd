754916b6396e8e6e72a084556e6590ce96dfbb74
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-04-19 16:46:20 -0300

    add connect wallet button and integrate it with the form

--- webapp/src/components/RootLayout.tsx
+++ webapp/src/components/RootLayout.tsx
@@ -1,19 +1,64 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Toaster } from "@bleu-fi/ui";
 import React from "react";
 import Fonts from "./Fonts";
+import merge from "lodash.merge";
+
 import { Footer } from "./Footer";
+import { WagmiProvider } from "wagmi";
+import { config } from "#/wagmi/client";
+import { RainbowKitProvider, Theme, darkTheme } from "@rainbow-me/rainbowkit";
+import { mainnet } from "viem/chains";
 
 export function RootLayout({ children }: React.PropsWithChildren) {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Fonts />
-      <div className="flex flex-col h-screen">
-        <div className="size-full">{children}</div>
-        <Footer />
-      </div>
-      <Toaster />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            initialChain={mainnet}
+            modalSize="compact"
+            theme={CustomTheme}
+          >
+            <Fonts />
+            <div className="flex flex-col h-screen">
+              <div className="size-full">{children}</div>
+              <Footer />
+            </div>
+            <Toaster />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   );
 }
+
+const CustomTheme = merge(darkTheme(), {
+  colors: {
+    accentColor: "accent",
+    accentColorForeground: "accent-foreground",
+    closeButtonBackground: "destructive",
+    connectButtonText: "primary",
+    closeButton: "destructive",
+    connectButtonTextError: "destructive",
+    connectionIndicator: "primary",
+    generalBorder: "border",
+    menuItemBackground: "white",
+    modalBackground: "foreground",
+    modalBorder: "border-background",
+    modalText: "background",
+    modalTextSecondary: "info",
+  },
+  fonts: {
+    body: "var(--font-family-sans)",
+  },
+  radii: {
+    menuButton: "25px",
+    modal: "25px",
+  },
+} as Theme);
---
+++ webapp/src/wagmi/client.ts
@@ -0,0 +1,29 @@
+import { getDefaultConfig, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+
+/**
+ * Project ID is required by Rainbowkit Migration Guide to Viem
+ * 2. Supply a WalletConnect Cloud projectId
+ * https://www.rainbowkit.com/docs/migration-guide#2-supply-a-walletconnect-cloud-projectid
+ * Credentials for WalletConnect are available at 1Password
+ */
+
+const appName = "Have I Been Mev'ed";
+const projectId = "4f98524b2b9b5a80d14a519a8dcbecc2";
+const chains = [mainnet] as const;
+
+const { wallets } = getDefaultWallets({
+  appName,
+  projectId,
+});
+
+export const config = getDefaultConfig({
+  appName,
+  projectId,
+  chains,
+  transports: {
+    [mainnet.id]: http(),
+  },
+  wallets,
+});

277450c4e5ead242de6324a4f0404f67db66dc19
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-04-26 10:58:43 -0300

    refactor footer layout

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -1,27 +1,36 @@
 export function Footer() {
   return (
-    <footer className="flex w-full justify-between p-4 text-foreground/90 text-sm">
+    <footer className="flex w-full justify-between text-foreground/90 text-md py-4 border-t-2">
       <span>
         developed by&nbsp;
         <a
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/bleu-fi"
-          className="font-bold text-primary"
+          className="font-bold text-sky-600"
         >
           bleu
+        </a>
+        &nbsp;using data from&nbsp;
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://zeromev.org/"
+          className="font-bold"
+        >
+          zeromev
         </a>
       </span>
       <div className="float-right flex flex-row items-center space-x-4">
         <div>
-          powered by &nbsp;
+          Visit&nbsp;
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://zeromev.org/"
-            className="font-bold text-accent"
+            href="https://cow.fi/"
+            className="font-bold underline underline-offset-4"
           >
-            zeromev
+            CoW Swap
           </a>
         </div>
       </div>
